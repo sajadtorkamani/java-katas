530d00d5320862134ce31e70e76493b03b20f49c
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-11-18 09:17:32 +0000

    wip

--- src/main/java/playground/StringPlayground.java
+++ src/main/java/playground/StringPlayground.java
@@ -2,17 +2,28 @@ package playground;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Optional;
 
 public class StringPlayground {
     public static void main(String[] args) throws URISyntaxException {
         var url = "https://example.com/auth/callback?post_login_redirect=https%253A%2F%2Fapp.rqratings.dev%2F&foo=bar";
+        System.out.println(extractQueryParam(url, "foo"));
+    }
 
+    private static String extractQueryParam(String url, String paramName) throws URISyntaxException {
         var uri = new URI(url);
         var query = uri.getQuery();
 
         for (var param : query.split("&")) {
-            String[] paramKeyAndValue = param.split("=", 2);
-            System.out.printf("%s: %s%n", paramKeyAndValue[0], paramKeyAndValue[1]);
+            var keyAndValue = param.split("=", 2);
+            String key = keyAndValue[0];
+            String value = keyAndValue[1];
+
+            if (key.equals(paramName)) {
+                return value;
+            }
         }
+
+        return null;
     }
 }
