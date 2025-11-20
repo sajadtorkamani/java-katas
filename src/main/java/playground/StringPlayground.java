068284a5803b49b5de5b2c9f5fb9255b7bcb5664
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-11-20 08:25:40 +0000

    wip

--- src/main/java/playground/StringPlayground.java
+++ src/main/java/playground/StringPlayground.java
@@ -8,9 +8,10 @@ import java.nio.charset.StandardCharsets;
 public class StringPlayground {
     public static void main(String[] args) throws URISyntaxException {
         var validUrl = "https://api.rqratings.dev/auth/callback?post_login_redirect=https%253A%2F%2Fapp.example.dev%2Fcompass%2Fredirect%253FreportToPrefetch%253D4973fcee-5c47-4553-a752-b7a24c10531b%2526returnUrl%253Dhttps%25253A%25252F%25252Fapp.example.dev%25252Fcompass%25252Fdashboard";
+        var invalidUrl = "";
 
         try {
-            System.out.println(extractQueryParam(validUrl, "post_login_redirect"));
+            System.out.println(extractQueryParam(invalidUrl, "post_login_redirect"));
         } catch (URISyntaxException $exception) {
             System.out.println("post_login_redirect is invalid");
         }
@@ -20,6 +21,10 @@ public class StringPlayground {
     private static String extractQueryParam(String url, String paramName) throws URISyntaxException {
         var uri = new URI(url);
         var query = uri.getQuery();
+
+        if (query == null) {
+            return null;
+        }
 
         for (var param : query.split("&")) {
             var keyAndValue = param.split("=", 2);
