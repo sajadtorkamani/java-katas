8740e1073f8a6747bbbb766bc0c59d054eac736d
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-11-20 08:43:49 +0000

    wip

--- src/main/java/playground/StringPlayground.java
+++ src/main/java/playground/StringPlayground.java
@@ -7,8 +7,8 @@ import java.nio.charset.StandardCharsets;
 
 public class StringPlayground {
     public static void main(String[] args) throws URISyntaxException {
-        var validUrl = "https://api.rqratings.dev/auth/callback?post_login_redirect=https%253A%2F%2Fapp.example.dev%2Fcompass%2Fredirect%253FreportToPrefetch%253D4973fcee-5c47-4553-a752-b7a24c10531b%2526returnUrl%253Dhttps%25253A%25252F%25252Fapp.example.dev%25252Fcompass%25252Fdashboard";
-        var invalidUrl = "";
+        var validUrl = "https://api.example.dev/auth/callback?post_login_redirect=https%253A%2F%2Fapp.example.dev%2Fcompass%2Fredirect%253FreportToPrefetch%253D4973fcee-5c47-4553-a752-b7a24c10531b%2526returnUrl%253Dhttps%25253A%25252F%25252Fapp.example.dev%25252Fcompass%25252Fdashboard";
+        var invalidUrl = "https://example.com?post_login_redirect";
 
         try {
             System.out.println(extractQueryParam(invalidUrl, "post_login_redirect"));
@@ -28,6 +28,11 @@ public class StringPlayground {
 
         for (var param : query.split("&")) {
             var keyAndValue = param.split("=", 2);
+
+            if (keyAndValue.length != 2) {
+                continue;
+            }
+
             String key = keyAndValue[0];
             String value = keyAndValue[1];
 
